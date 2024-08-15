fd2db73f156b957e063279b4c73ac6dea00c50c5
bsswd <alexandersinkin@mail.ru>
2024-08-15 14:47:04 +0700

    HW8
    HealthComponent restored

---
+++ Plugins/Health/Source/Health/Private/HealthComponent.cpp
@@ -0,0 +1,94 @@
+#include "HealthComponent.h"
+#include "Health.h"
+
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!GetOwner())
+		return;
+
+	CurrentHealth = MaxHealth;
+
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeAnyDamage);
+}
+
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHealthComponent::TakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	
+	ReceiveDamage(Damage);
+}
+
+void UHealthComponent::ReceiveDamage(float Damage)
+{
+	if (bDead)
+		return;
+
+	if (Damage < 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UHealthComponent::ReceiveDamage(): Damage < 0"));
+		return;
+	}
+
+	Damage = Damage - Damage * ArmorValue;
+	CurrentHealth = FMath::Max(0.f, (CurrentHealth - Damage));
+	
+	if (Damage > 0)
+		OnHealthChange.Broadcast(CurrentHealth);
+
+	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, FString::Printf(TEXT("CurrentHealth %f"), CurrentHealth));
+
+	if (FMath::IsNearlyEqual(CurrentHealth, 0.f, 0.01f))
+	{
+		Death();
+	}
+}
+
+bool UHealthComponent::Heal(float HealValue)
+{
+	if (HealValue <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UHealthComponent::Heal(): HealValue <= 0"));
+		return false;
+	}
+
+	if (CurrentHealth >= MaxHealth)
+		return false;
+
+	CurrentHealth = FMath::Min(MaxHealth, (CurrentHealth + HealValue));
+	OnHealthChange.Broadcast(CurrentHealth);
+	return true;
+}
+
+void UHealthComponent::SetMaxHealth(float NewMaxHealth)
+{
+	if (NewMaxHealth <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UHealthComponent::SetMaxHealth(): NewMaxHealth <= 0"));
+		return;
+	}
+
+	MaxHealth = NewMaxHealth;
+}
+
+void UHealthComponent::Death()
+{
+	bDead = true;
+
+	OnDeath.Broadcast();
+
+	if (bDestroyActorOnDeath)
+		GetOwner()->Destroy();
+}
---
+++ Plugins/Health/Source/Health/Public/HealthComponent.h
@@ -0,0 +1,68 @@
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeath);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChange, float, CurrentHealth);
+
+
+UCLASS( ClassGroup=(Health), meta=(BlueprintSpawnableComponent) )
+class HEALTH_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHealthComponent();
+
+	UPROPERTY(BlueprintAssignable)
+	FOnDeath OnDeath;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthChange OnHealthChange;
+
+protected:
+	virtual void BeginPlay() override;
+
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	void TakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+		
+	UFUNCTION(BlueprintCallable)
+	void ReceiveDamage(float Damage);
+
+	UFUNCTION(BlueprintCallable)
+	bool Heal(float HealValue);
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE float GetCurrentHealth() const { return CurrentHealth; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE bool IsDead() const { return bDead; }
+
+	UFUNCTION(BlueprintCallable)
+	void SetMaxHealth(float NewMaxHealth);
+
+private:
+	void Death();
+
+	UPROPERTY(EditAnywhere, Category = "Health", meta = (AllowPrivateAccess = "true"))
+	float MaxHealth = 100.f;
+
+	UPROPERTY(EditAnywhere, Category = "Death", meta = (AllowPrivateAccess = "true"))
+	bool bDestroyActorOnDeath = false;
+
+	float CurrentHealth;
+
+	UPROPERTY(EditAnywhere, Category = "Health", meta = (AllowPrivateAccess = "true", ClampMin = 0, ClampMax = 1))
+	float ArmorValue;
+
+	bool bDead = false;
+};
